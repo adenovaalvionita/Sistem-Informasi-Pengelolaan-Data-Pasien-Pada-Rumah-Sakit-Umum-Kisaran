4bce533917f218c1444fb9cf490447759132c81c
adenovaalvionita <60249848+adenovaalvionita@users.noreply.github.com>
2020-01-24 14:53:46 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,39 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-success">
+           Selamat datang kembali <strong><?=$_SESSION['nama']?></strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-warning">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Administrator Sistem Informasi Pengelolaan Data Pasien Rumah Sakit Umum Kisaran</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/RSU.jpg"></img></p>
+                          <p class="label-title" align="center"><strong>RUMAH SAKIT UMUM KISARAN</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator Sistem Informasi Pengelolaan Data Pasien Rumah Sakit Umum Kisaran.<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data Pasien, pengelolaan data transaksi, <br>
+                            dan juga pengelolaan laporan Pasien serta laporan transaksi.<br><br>
+                            Akses menu Master Data pada bagian atas sistem untuk pengelolaan data Pasien dan data transaksi.<br>
+                            Untuk mengelola laporan, dapat dilakukan dengan mengakses menu report pada bagian atas sistem.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                         </thead>
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
